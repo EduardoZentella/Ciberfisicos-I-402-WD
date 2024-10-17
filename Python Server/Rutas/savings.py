ea6b878bdee3882c203bc73f51c9c619890dd4a0
DIegoTome <A01733345@tec.mx>
2024-10-16 21:39:54 -0600

    hola

--- Python Server/Rutas/savings.py
+++ Python Server/Rutas/savings.py
@@ -1,3 +1,44 @@
-# Rutas/savings.py
+# Rutas/savings.pyç
 from flask import request, jsonify # Importar las librerías necesarias de Flask
 from App.application import app, db, get_first_level # Importar la aplicación de Flask y la instancia de Firebase
+import datetime
+
+# Ruta para crear un nuevo recurso
+@app.route('/<path:path>', methods=['POST'])
+
+#request me da el startdate, lo agarro del request json, calculo el endate con la funcion con rectificar el formato, hacder un reference de fechas
+#me regresa un diccionario con las fechas, filtrar para que me de las fechas del rango y con eso tengo todos los paths 
+#guardarlo en flujo list los que vaya agarrando en esas fechas
+
+def create(path):
+    data = request.json
+    ref = db.reference(f'/{path}')
+    return jsonify(data), 201
+
+def calculatePro():
+    startDate = datetime.datetime.now()
+    endDate = None
+    formats = ["%Y-%m-%d", "%Y-%m", "%Y"]
+    formatUsed = None
+    for fmt in formats:
+        try:
+            if fmt == startDate.strftime(format/fmt):
+                formatUsed = fmt
+                break
+        except ValueError:
+            continue
+    
+match formatUsed:
+    case "%Y-%m-%d":
+        endDate = startDate + datetime.timedelta(days=1)
+    case "%Y-%m":
+        endDate = startDate + datetime.timedelta(days=30)
+    case "Y":
+        endDate = startDate + datetime.timedelta(days=365)
+endDate -= datetime.timedelta(milliseconds=1)
+
+
+
+
+
+
